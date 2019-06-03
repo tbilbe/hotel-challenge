39c40c90d99acf418b8a1dafe99937ff8906edd9
tbilbe <tbilbe@gmail.com>
2019-06-03 19:54:24 +0100

    updated users route with form POST

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 const MongoClient = require('mongodb');
 
 const app = express();
 const router = express.Router();
 let db;
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(router);
+
 const DATABASE_CONN =
   'mongodb://admin:password1@ds263856.mlab.com:63856/hotels-r-us';
-mongoose.connect(DATABASE_CONN, {
-  useNewUrlParser: true,
-});
+// mongoose.connect(DATABASE_CONN, {
+//   useNewUrlParser: true,
+// });
 
-MongoClient.connect(DATABASE_CONN, (err, database) => {
+MongoClient.connect(DATABASE_CONN, {
+  useNewUrlParser: true,
+}, (err, database) => {
   if (err) {
     console.log(err);
   } else {
     db = database.db('hotels-r-us');
-    app.listen(3000, () => console.log('running mofo, on: port 3000'));
+    app.listen(3010, () => console.log('running mofo, on: port 3010'));
   }
 });
 
-app.use(router);
 
 app.post('/hotels', (req, res) => {
-  // const {name, email, address} = req.body
   db.collection('hotels').save(req.body, (err, result) => {
     if (err) return console.log(err);
-    console.log('i think we saved something to the db!');
+    console.log('We successfully signed up a hotel!');
     res.json(result);
   });
 });
 
+app.get('/users', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/userForm.html'));
+})
+
 app.post('/users', (req, res) => {
-  res.status(201).json();
+  const {
+    name,
+    email
+  } = req.body;
+  // res.sendFile(path.join(__dirname + '/public/success.html'));
+  db.collection('users').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+    console.log(req.body)
+    console.log('we got a user signed up successfully')
+    res.status(201).json(result);
+  });
 });
 
 module.exports = app;
